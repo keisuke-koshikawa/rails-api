807a4cbc74af9961ece331d334f744befab1cb38
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-04-27 20:25:44 +0900

    add columns for users

--- db/migrate/20210427112242_devise_token_auth_create_users.rb
+++ db/migrate/20210427112242_devise_token_auth_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
   def change
-    
+
     create_table(:users) do |t|
       ## Required
       t.string :provider, :null => false, :default => "email"
@@ -16,6 +16,13 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
 
       ## Rememberable
       t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       t.string   :confirmation_token
